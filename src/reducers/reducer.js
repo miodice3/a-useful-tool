7343d59cfdc29a6744143603da75b1259c84aaeb
Michael Iodice <miodice3@gmail.com>
2021-03-02 13:59:52 -0700

    Revert "remove old code from reducer"
    This reverts commit e7dda947c014524f9262242dfb034a612fc3024b.

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,5 +1,6 @@
 
-export default function reducer(state={ vehicles: {} }, action){    switch (action.type){
+export default function reducer(state={ vehicles: {} }, action){ // todo: this should run without vehicle_a: {}, vehicle_b: {}
+    switch (action.type){
         case 'START_ADD_FORECAST_REQUEST':
             return {
                 ...state,
@@ -11,7 +12,7 @@ export default function reducer(state={ vehicles: {} }, action){    switch (acti
                 forecasts: action.payload.map(schedule => schedule),
                 requesting: false
                 }
-
+// *****************************************************************
         case 'START_GET_YEAR_REQUEST':
 
             var newState = {
@@ -57,6 +58,8 @@ export default function reducer(state={ vehicles: {} }, action){    switch (acti
             }
             return newState;
 
+// *****************************************************************
+
         case 'START_GET_MANUFACTURER_REQUEST':
             var newState = {...state}
 
@@ -98,8 +101,11 @@ export default function reducer(state={ vehicles: {} }, action){    switch (acti
             newState.vehicles[action.selector] = {
                 ...vehicle,
                 selectedManufacturer: action.payload
+                // selectedManufacturer: "BMW"
             }
             return newState;
+
+// *****************************************************************
 
         case 'START_GET_MODEL_REQUEST':
             var newState = {...state}
@@ -141,8 +147,11 @@ export default function reducer(state={ vehicles: {} }, action){    switch (acti
                 newState.vehicles[action.selector] = {
                     ...vehicle,
                     selectedModel: action.payload
+                    // selectedManufacturer: "BMW" << needs to be a list item of models to test successfully
                 }
                 return newState;
+
+// *****************************************************************
 
         case 'START_GET_VEHICLE_REQUEST':
             var newState = {...state}
@@ -158,6 +167,13 @@ export default function reducer(state={ vehicles: {} }, action){    switch (acti
                 fedID_number: null
             }
             return newState
+
+            // return {
+            //     ...state,
+            //     vehicle_a_detail_requested: false,
+            //     requesting_vehicle: true,
+            //     requesting_detail: false
+            // } 
 
         case 'ADD_VEHICLE':
 
@@ -175,6 +191,13 @@ export default function reducer(state={ vehicles: {} }, action){    switch (acti
             }
             return newState
 
+            // return {
+            //     ...state,
+            //     vehicle: action.payload.menuItems.menuItem[0].value[0],
+            //     requesting_vehicle: false,
+            //     requesting_detail: false
+            //     }
+
             case 'START_GET_DETAIL_REQUEST':
                 var newState = {...state}
 
@@ -189,6 +212,11 @@ export default function reducer(state={ vehicles: {} }, action){    switch (acti
                 }
 
                 return newState
+
+            // return {
+                //     ...state,
+                //     requesting_detail: true
+                // } 
     
             case 'ADD_DETAIL':
                 // debugger
@@ -212,6 +240,9 @@ export default function reducer(state={ vehicles: {} }, action){    switch (acti
                                 selectedManufacturer: action.payload.vehicle.make[0],
                                 selectedModel: action.payload.vehicle.model[0],
                                 vehicle_detail_requested: true,
+                                // mfg: action.payload.vehicle.make[0],
+                                // model: action.payload.vehicle.model[0],
+                                // year: action.payload.vehicle.year[0],
                                 vehicle_emissions: mpkwh,
                                 vehicle_fuel_type: action.payload.vehicle.fuelType[0],
                                 requesting_detail: false
@@ -224,6 +255,9 @@ export default function reducer(state={ vehicles: {} }, action){    switch (acti
                                     selectedManufacturer: action.payload.vehicle.make[0],
                                     selectedModel: action.payload.vehicle.model[0],
                                     vehicle_detail_requested: true,
+                                    // mfg: action.payload.vehicle.make[0],
+                                    // model: action.payload.vehicle.model[0],
+                                    // year: action.payload.vehicle.year[0],
                                     vehicle_emissions: action.payload.vehicle.co2TailpipeGpm[0],
                                     vehicle_fuel_type: action.payload.vehicle.fuelType[0],
                                     requesting_detail: false
@@ -232,6 +266,73 @@ export default function reducer(state={ vehicles: {} }, action){    switch (acti
                         }
 
                 return newState
+                // }
+
+                // if (action.selector === "A"){
+                //     console.log("WITHIN UPDATE DETAIL FOR A")
+                //     if (action.payload.vehicle.fuelType[0] === "Electricity"){
+                //         watthourspermile = (action.payload.vehicle.combE[0] * 1000 / 100) //kwh to watt hours, divided by 100 miles
+                //         mpkwh = 1000 / watthourspermile
+                //         return {
+                //             ...state,
+                //             vehicle_a: {
+                //                     vehicle_detail_requested: true,
+                //                     mfg: action.payload.vehicle.make[0],
+                //                     model: action.payload.vehicle.model[0],
+                //                     year: action.payload.vehicle.year[0],
+                //                     vehicle_emissions: mpkwh,
+                //                     vehicle_fuel_type: action.payload.vehicle.fuelType[0],
+                //                     requesting_detail: false
+                //                 }
+                //             }
+                //     } else {
+                //         return {
+                //             ...state,
+                //             vehicle_a: {
+                //                 vehicle_detail_requested: true,
+                //                 mfg: action.payload.vehicle.make[0],
+                //                 model: action.payload.vehicle.model[0],
+                //                 year: action.payload.vehicle.year[0],
+                //                 vehicle_emissions: action.payload.vehicle.co2TailpipeGpm[0],
+                //                 vehicle_fuel_type: action.payload.vehicle.fuelType[0],
+                //                 requesting_detail: false
+                //             }
+                //         }
+                //     }
+                // } else if (action.selector === "B"){
+                //     console.log("WITHIN UPDATE DETAIL FOR B")
+                //     if (action.payload.vehicle.fuelType[0] === "Electricity"){
+                //         watthourspermile = (action.payload.vehicle.combE[0] * 1000 / 100) //kwh to watt hours, divided by 100 miles
+                //         mpkwh = 1000 / watthourspermile
+                //         return {
+                //             ...state,
+                //             vehicle_b: {
+                //                     vehicle_detail_requested: true,
+                //                     mfg: action.payload.vehicle.make[0],
+                //                     model: action.payload.vehicle.model[0],
+                //                     year: action.payload.vehicle.year[0],
+                //                     vehicle_emissions: mpkwh,
+                //                     vehicle_fuel_type: action.payload.vehicle.fuelType[0],
+                //                     requesting_detail: false
+                //                 }
+                //             }
+                //     } else {
+                //         return {
+                //             ...state,
+                //             vehicle_b: {
+                //                 vehicle_detail_requested: true,
+                //                 mfg: action.payload.vehicle.make[0],
+                //                 model: action.payload.vehicle.model[0],
+                //                 year: action.payload.vehicle.year[0],
+                //                 vehicle_emissions: action.payload.vehicle.co2TailpipeGpm[0],
+                //                 vehicle_fuel_type: action.payload.vehicle.fuelType[0],
+                //                 requesting_detail: false
+                //             }
+                //         }
+                //     }
+                // } else {
+                //     return state;
+                // }
 
         default:
             return state;
