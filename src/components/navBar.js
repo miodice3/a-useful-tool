164d7a709f2974998ac7ac31edace2cd738555c1
Michael Iodice <miodice3@gmail.com>
2021-03-08 14:08:15 -0700

    true up to switch branch

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,15 +2,12 @@ function NavBar() {
         return (
             <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="container">
-                <a class="navbar-brand" href="index.html">An Informative Tool</a>
+                <a class="navbar-brand" href="/">An Informative Tool</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-                    </li>
                     <li class="nav-item">
                     <a class="nav-link" href="/nationalgrid">Britain's National Grid Data</a>
                     </li>
@@ -25,6 +22,3 @@ function NavBar() {
     }
 
   export default NavBar;
-
-
-  
