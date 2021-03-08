10e4f41bd3e65768e261280ed5729c8140847597
Michael Iodice <miodice3@gmail.com>
2021-03-08 15:47:12 -0700

    correct footer spelling error

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ function Footer() {
 
         <footer class="py-5 bg-dark">
           <div class="container">
-            <p class="m-0 text-center text-white">An Inforative Tool 2021</p>
+            <p class="m-0 text-center text-white">An Informative Tool 2021</p>
           </div>
         </footer>
 
