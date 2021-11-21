9311d3254212537498838cbcd9c0184bbb668ba1
michael <michael.iodice@smartersorting.com>
2021-11-20 19:28:44 -0700

    fix: work in progress form basics work

--- src/components/mfgWeight.js
+++ src/components/mfgWeight.js
@@ -3,12 +3,11 @@ import React, { PureComponent} from 'react';
 class MfgWeight extends PureComponent {
 
   state = {
-    mfgWeight: undefined
   }
 
   mfgWeight = (event) => {
     this.setState({
-      mfgWeight: parseInt(event.target.value)
+      mfgWeight: (event.target.value ? event.target.value : 0)
     })
   }
 
@@ -25,8 +24,7 @@ class MfgWeight extends PureComponent {
         <label>
             Input weight of CO2 used to mfg automobile in KG
         </label><br/>
-        <input type="text"
-        pattern="[0-9]*"
+        <input type="number"
         onChange={this.mfgWeight}
         value={this.state.mfgWeight}
         placeholder="12500"
