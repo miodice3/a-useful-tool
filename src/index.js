3fa95796d5cff538f57da9b66497a39a2e8c2c45
michael <michael.iodice@smartersorting.com>
2021-11-21 06:54:54 -0700

    fix: index config

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ import rootReducer from './reducers/reducer';
 
 import logger from 'redux-logger'
 
-import { createStore, applyMiddleware, compose } from 'redux';
-const store = createStore(rootReducer, compose(
-  applyMiddleware(thunk, logger),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-)
+// import { createStore, applyMiddleware, compose } from 'redux';
+// const store = createStore(rootReducer, compose(
+//   applyMiddleware(thunk, logger),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// )
 
 // FOR DEVELOPMENT, TEST ADD ROOTREDUCER
-// import { createStore, applyMiddleware } from 'redux';
-// const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+import { createStore, applyMiddleware } from 'redux';
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
 
 // const store = createStore(reducer, applyMiddleware(thunk, logger))
 
